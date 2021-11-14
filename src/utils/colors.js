c58e3a0ad5f71c2cf9e5f43c145142a949e5b2e4
Stefan Krüger <stefan-krueger@hotmail.de>
2021-11-14 02:38:14 +0100

    add multiple materials

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,11 +1,17 @@
 export const Colors = {
     black: 0x000000,
     white: 0xFFFFFF,
+    paper: 0xcbcbbd,
+    pen: 0x3701e9,
     gold: 0xa69326,
     metal: 0x757575,
     darkMetal: 0x3d3d3d,
     blackMetal: 0x0f0f0f,
+    blackestMetal: 0x050505,
     sun: 0xf5c800,
     orange: 0xd06201,
-    wood: 0x361a11
+    wood: 0x361a11,
+    brightGreen: 0x15d100,
+    green: 0x093d01,
+    purple: 0x9201b7
 };
--- src/utils/materials.js
+++ src/utils/materials.js
@@ -26,7 +26,32 @@ export const Materials = {
     // specular: Colors.sun,
     // shininess: 60,
   }),
+  darkMetalBasic: new THREE.MeshBasicMaterial({
+    color: Colors.darkMetal,
+    // emissive: Colors.black,
+    // specular: Colors.sun,
+    // shininess: 60,
+  }),
+  blackMetalBasic: new THREE.MeshBasicMaterial({
+    color: Colors.blackMetal,
+  }),
+  blackMetalStandard: new THREE.MeshStandardMaterial({
+    color: Colors.blackestMetal,
+  }),
+  paperStandard: new THREE.MeshStandardMaterial({
+    color: Colors.paper,
+  }),
+  penStandard: new THREE.MeshStandardMaterial({
+    color: Colors.pen,
+    roughness: 0
+  }),
   wood: new THREE.MeshPhongMaterial({
     color: Colors.wood,
+  }),
+  green: new THREE.MeshStandardMaterial({
+    color: Colors.green,
+  }),
+  purple: new THREE.MeshPhongMaterial({
+    color: Colors.purple,
   }),
 };
